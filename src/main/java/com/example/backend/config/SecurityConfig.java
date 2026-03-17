1935e406f7867cf88fce61734f4618439b7b1929
pierre200326 <nichelepierre@gmail.com>
2026-03-17 11:35:07 +0100

    autoriser l'app front à accéder à l'api

--- src/main/java/com/example/backend/config/SecurityConfig.java
+++ src/main/java/com/example/backend/config/SecurityConfig.java
@@ -49,7 +49,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("http://localhost:4200"));
+        cfg.setAllowedOrigins(List.of(
+    "http://localhost:4200", // local dev
+    "https://pfe-digital-hub-frontend.vercel.app" // prod
+));
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(true);
